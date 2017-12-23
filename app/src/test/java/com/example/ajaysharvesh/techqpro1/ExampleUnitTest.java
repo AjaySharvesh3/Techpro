7d3c89d57beb370ddab80901444adb8c2ac13e70
AjaySharvesh3 <ajaysharvesh.mp@gmail.com>
2017-12-23 09:50:18 +0530

    Commit on 23/12

--- app/src/test/java/com/example/ajaysharvesh/techqpro1/ExampleUnitTest.java
+++ app/src/test/java/com/example/ajaysharvesh/techqpro1/ExampleUnitTest.java
@@ -1,15 +1,15 @@
-package com.wolfpack.ajaysharvesh.techqpro1;
+package com.wolfpackalpha.ajaysharvesh.techqpro1;
 
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 /**
- * wolfpack local unit test, which will execute on the development machine (host).
+ * wolfpackalpha local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class wolfpackUnitTest {
+public class wolfpackalphaUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
